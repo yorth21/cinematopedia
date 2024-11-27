b48862ebd9cb0339861391d0ff74240487049083
Yorth H. Ortegon <yorth@udenar.edu.co>
2024-11-27 17:19:09 -0500

    environment variables configured

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:cinematopedia/config/router/app_router.dart';
 import 'package:cinematopedia/config/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinematopedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +9,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
-    );
+        body: Center(
+      child: Text(Environment.movieDbKey),
+    ));
   }
 }
