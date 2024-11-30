fc45ec8b24bfa716cca0bea48e4fcc2a2b5f92bf
Yorth H. Ortegon <yorth@udenar.edu.co>
2024-11-29 22:27:24 -0500

    feat: mostar carrusel peliculas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinematopedia/config/constants/environment.dart';
 import 'package:cinematopedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinematopedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -37,15 +37,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       return const Center(child: CircularProgressIndicator());
     }
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        CustomAppbar(),
+        MoviesSlideshow(
+          movies: nowPlayingMovies,
+        )
+      ],
     );
   }
 }
